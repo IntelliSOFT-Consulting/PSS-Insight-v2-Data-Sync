9add8814a70fe75a423e66189b48e56fc989f607
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-06-16 10:19:09 +0300

    Add API call to fetch data_element list, add env variables

--- pss-insight-data-sync.py
+++ pss-insight-data-sync.py
@@ -1,5 +1,9 @@
 import requests
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Variables
 source_username = os.getenv("SOURCE_USERNAME")
@@ -8,13 +12,18 @@ target_username = os.getenv("TARGET_USERNAME")
 target_password = os.getenv("TARGET_PASSWORD")
 source_url = os.getenv("SOURCE_URL")
 target_url = os.getenv("TARGET_URL")
-
+data_elements_url = os.getenv("SOURCE_DATA_ELEMENTS_URL")
 
 # Check if any of the environment variables are missing
 if not (source_username and source_password and source_url and target_url):
-    raise ValueError("Please set the DHIS2_USERNAME, DHIS2_PASSWORD, SOURCE_URL, and TARGET_URL environment variables.")
+    raise ValueError("Please set the SOURCE_USERNAME, SOURCE_PASSWORD, SOURCE_URL, and TARGET_URL environment variables.")
 
-data_elements = ["Fz75Jo6v99X", "DAvT1ODxYKK", "HLBBj5vnCC3"]  # Specify the data elements to be included
+# Fetch data elements from source DHIS2 instance
+response = requests.get(data_elements_url, auth=(source_username, source_password))
+data_elements = response.json()["dataElements"]
+
+# Extract data element IDs
+data_element_ids = [data_element["id"] for data_element in data_elements]
 
 # Fetch data from source DHIS2 instance
 response = requests.get(source_url, auth=(source_username, source_password), params={"paging": "false"})
@@ -31,11 +40,11 @@ for event in data["events"]:
         "dataValues": []
     }
     for data_value in event["dataValues"]:
-        if data_value["dataElement"] in data_elements:
+        if data_value["dataElement"] in data_element_ids:
             new_event["dataValues"].append(data_value)
     processed_data.append(new_event)
 
-print('processed_data',processed_data)
+print('processed_data', processed_data)
 
 # Push processed data to target DHIS2 instance
 for event in processed_data:
